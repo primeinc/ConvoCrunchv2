f2b9ff84ed487b183c279a664955e092d04ef5ec
primeinc <4395149+primeinc@users.noreply.github.com>
2023-01-29 23:16:37 -0500

    all basic libraries working as expected

--- chat_processor.py
+++ chat_processor.py
@@ -1,26 +1,28 @@
 import pandas as pd
 import nltk
-import pytextrank
 import spacy
+import pytextrank
 from textblob import TextBlob
-from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lex_rank import LexRankSummarizer
-from spacy.lang.en.stop_words import STOP_WORDS
-from nltk.tokenize import sent_tokenize
-from pytextrank import parse_doc, pretty_print
-# from nltk import TextRank
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+from sumy.models.dom import ObjectDocumentModel
+from pprint import pprint
 
-nltk.download('vader_lexicon')
-nltk.download('punkt')
+# nltk.download('vader_lexicon')
+# nltk.download('punkt')
 
 def process_chat_data(csv_file: str):
+    LANGUAGE = "english"
+    SENTENCES_COUNT = 3
     # Test that the file passed as an argument exists and is a file
     test_process_chat_data(csv_file)
     # Read the CSV file into a DataFrame and parse the Message Date column as a datetime object
     df = pd.read_csv(csv_file, parse_dates=['Message Date','Delivered Date','Read Date','Edited Date'])
     # Convert the Message Date column to a datetime object
-    df['conversation_id'] = df['Service'] + '_' + df['Type'] + '_' + df['Message Date'].dt.date.astype(str)
+    df['conversation_id'] = df['Service'] + '_' + df['Message Date'].dt.date.astype(str)
 
 
     # Perform the necessary processing on the data
@@ -29,6 +31,7 @@ def process_chat_data(csv_file: str):
     from nltk.sentiment import SentimentIntensityAnalyzer
 
     sia = SentimentIntensityAnalyzer()
+    stemmer = Stemmer('english')
 
     for conversation_id, messages in conversations:
         text = messages['Text'].str.cat(sep=' ')
@@ -37,29 +40,34 @@ def process_chat_data(csv_file: str):
         print(f'Conversation ID: {conversation_id}')
         print(f'Sentiment: {sentiment}, keywords: {keywords}')
 
+        # Tokenize text
+        parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+
         # Sumy LsaSummarizer
-        summarizer = LsaSummarizer(Stemmer('english'))
+        summarizer = LsaSummarizer(stemmer)
         summarizer.stop_words = get_stop_words('english')
-        summary = ' '.join(map(str, summarizer(Tokenizer('english').to_words(text), 5)))
+        summary = ' '.join(map(str, summarizer(parser.document, SENTENCES_COUNT)))
         print(f'Summary (Sumy LsaSummarizer): {summary}')
 
-        # PyTextRank
-        nlp = spacy.load("en_core_web_sm")
-        tr = TextRank()
-        nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
-        doc = nlp(text)
-        summary = ' '.join([sent.text for sent in doc._.textrank.summary(normalize=True)])
-        print(f'Summary (PyTextRank): {summary}')
-
         # Spacy TextRank
-        nlp = spacy.load("en_core_web_sm")
-        doc = nlp(text)
-        summary = ' '.join([sent.text for sent in doc._.textrank_summary(normalize=True)])
+        nlp = spacy.load("en_core_web_lg")
+        # nlp = spacy.load("en_core_web_sm")
+        summary = extract_sentences(text, nlp, 3)
         print(f'Summary (Spacy TextRank): {summary}')
 
+        # PyTextRank
+        nlp2 = spacy.load("en_core_web_lg")
+        # add PyTextRank to the spaCy pipeline
+        nlp2.add_pipe("textrank")
+        # examine the top-ranked phrases in the document        
+        summary = extract_sentences(text, nlp2, 3)
+        print(f'Summary (PyTextRank): {summary}')
+
         # NLTK TextRank
-        summary = summarize(text, ratio=0.2)
-        print(f'Summary (NLTK TextRank): {summary}')
+        parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+        summarizer = LsaSummarizer()
+        summary = summarizer(parser.document, 5)
+        print(f'Summary (NLTK Sumy): {summary}')
 
 
 import os
@@ -70,3 +78,60 @@ def test_process_chat_data(csv_file: str):
 
     # Test that the file passed as an argument is a CSV file
     assert csv_file.endswith('.csv') == True
+
+from math import sqrt
+
+def extract_sentences(text, model, number):
+    if text:
+        # get important sentences from description with the help of spaCy
+        doc = model(text)
+        # Doc.set_extension('phrases', default=True, force=True)
+        sent_bounds = [[sent.start, sent.end, set([])] for sent in doc.sents]
+        limit_phrases = 4
+        phrase_id = 0
+        unit_vector = []
+        # add phrases
+        phrases = []
+        # Check if ._.phrases exists and only overwrite when len() == 0
+        if not doc.has_extension('phrases') or (
+            doc.has_extension('phrases') and len(doc._.phrases) == 0
+        ):
+            doc.set_extension('phrases', default={}, force=True)
+            doc._.phrases = phrases
+        # examine top-ranked phrases
+        for phrase in doc._.phrases:
+            unit_vector.append(phrase.rank)
+            for chunk in phrase.chunks:
+                # construct a list of the sentence boundaries
+                for sent_start, sent_end, sent_vector in sent_bounds:
+                    # iterate through the top-ranked phrases, add them to the vector for each sentence
+                    if chunk.start >= sent_start and chunk.end <= sent_end:
+                        sent_vector.add(phrase_id)
+                        break
+            phrase_id += 1
+            if phrase_id >= limit_phrases:
+                break
+        sum_ranks = sum(unit_vector)
+        # normalize the vector
+        unit_vector = [rank / sum_ranks for rank in unit_vector]
+        sent_rank = []
+        sent_id = 0
+        # iterate through the sentences, add their euclidean distance from the unit vector
+        for sent_start, sent_end, sent_vector in sent_bounds:
+            sum_sq = 0.0
+            for phrase_id in range(len(unit_vector)):
+                if phrase_id not in sent_vector:
+                    sum_sq += unit_vector[phrase_id] ** 2
+            sent_rank.append({'sent_id': sent_id, 'rank': sqrt(sum_sq), 'sentence': doc[sent_start:sent_end].text})
+            sent_id += 1
+        # sort the sentences by their rank
+        sent_rank = sorted(sent_rank, key=lambda x: x['rank'])
+        # extract the n most important sentences (with the lowest euclidean distance)
+        shortened_description = ''
+        for i in range(min(number, len(sent_rank))):
+            if shortened_description == '':
+                shortened_description = sent_rank[i]['sentence']
+            else:
+                shortened_description += ' ' + sent_rank[i]['sentence']
+        # shortened_description = sent_rank[0]['sentence'] + ' ' + sent_rank[1]['sentence']
+        return shortened_description
